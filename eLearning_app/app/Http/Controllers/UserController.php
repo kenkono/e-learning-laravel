8a033be93d6eb4db4dbb42948495d851e2e71c43
kenkono <twentynresult@yahoo.co.jp>
2019-08-15 12:00:41 +0800

    edit profile

---
+++ eLearning_app/app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Auth;
+use App\User;
+
+class UserController extends Controller
+{
+    public function edit($id) {
+        $user = User::find($id);
+
+        return view('users.editUser', compact('user'));
+    }
+
+    public function editStore($id)
+    {
+        $image = request()->file('avatar');
+
+        $file = $image->getClientOriginalName();
+
+        $image->storeAs('public/images' , $file);
+
+        $user = Auth::user()->update([
+            'name' => request()->name,
+            'email' => request()->email,
+            'avatar' => '/storage/images/'.$file,
+        ]);
+
+        return redirect('home');
+    }
+}
--- eLearning_app/resources/views/home.blade.php
+++ eLearning_app/resources/views/home.blade.php
@@ -2,11 +2,17 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
+    <div class="row">
+        <!-- user profile -->
+        <div class="col-sm-4">
+            <div class="card user-profile">
+                <div class="card-body">
+                    <div class="text-center">
+                        <div class="avatar">
+                            <div class="default">
+                                 <img src="{{Auth::user()->avatar}}" style="width:100px;height:100px;">
+                            </div>
+                        </div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
