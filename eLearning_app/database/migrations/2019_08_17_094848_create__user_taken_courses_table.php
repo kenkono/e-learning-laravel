61e49fa372d3192a4e439ed73b1091c3f1b87ad7
kenkono <twentynresult@yahoo.co.jp>
2019-08-17 18:41:14 +0800

    make pivot

---
+++ eLearning_app/database/migrations/2019_08_17_094848_create__user_taken_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTakenCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('UserTakenCourses', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('lesson_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('lesson_id')->references('id')->on('lessons')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('UserTakenCourses');
+    }
+}
