57317ca32c7ed243daef32eee2e15bce6c0d391e
kenkono <twentynresult@yahoo.co.jp>
2019-08-21 17:02:04 +0800

    view page

--- eLearning_app/resources/views/lessons/question.blade.php
+++ eLearning_app/resources/views/lessons/question.blade.php
@@ -6,29 +6,62 @@
         <div class="row">
             <div class="col-sm-12">
                 <h1>{{$lesson->title}}</h1>
-                @foreach($questions as $question)
-                <div class="card">
-                    <div class="card-body">
-                        <div class="text-left">
-                            <h1>Question1</h1>
-                            <p>{{$question->question}}</p>
-                            <div class="radio">
-                                <label><input type="radio" name="optradio1" checked>Option 1</label>
+                <form action="/lessons/content/answer/{{$lesson->id}}" method="POST" class="m-10">
+                @csrf
+                    @foreach($lesson->questions as $question)
+                        @isset($question->correct)
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="text-left">
+                                    <p>The Correct Answer is : {{$question->correct}}</p>
+                                </div>
                             </div>
-                            <div class="radio">
-                                <label><input type="radio" name="optradio1">Option 2</label>
-                            </div>
-                            <div class="radio">
-                                <label><input type="radio" name="optradio1">Option 3</label>
+                        </div>
+                        @endif
+
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="text-left">
+                                <h1>Question</h1>
+                                <p>{{$question->question}}</p>
+
+                                @isset($question->correct)
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="text-left"> 
+                                            <p>{{$question->explanations->explanation}}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                @endif
+
+                                @foreach($question->choices as $choice)
+                                    <div 
+                                        class="radio {{ $question->user_answer == $choice->id ? $question->answer_color : '' }}">
+                                        <label>
+                                            <input 
+                                                type="radio" 
+                                                name="question[{{$question->id}}]" 
+                                                value="{{$choice->id}}"
+                                                {{ $question->user_answer == $choice->id ? "checked" : ""}}
+                                            >
+                                                {{$choice->choice}}
+                                        </label>
+                                    </div>
+                                @endforeach
                             </div>
                         </div>
                     </div>
-                </div>
-                <br>
-                @endforeach
-                <div class="text-right">
-                    <p><a href="/lessons/content/answer" class="btn btn-primary">Submit</a></p>
-                </div>
+                    <br>
+                    @endforeach
+
+                    @if(!isset($question->correct))
+                    <div class="text-right">
+                        <button class="create-post btn btn-primary mt-3" type="submit">Submit</button>
+                    </div>
+                    @endif
+
+                </form>
             </div>
         </div>
     </div>
