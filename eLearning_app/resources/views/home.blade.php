c84147783897e1bcf7a9448041545524842cafca
Roden Artuyo <roden.artuyo@sun-asterik.com>
2019-08-27 10:19:08 +0800

    .

--- eLearning_app/resources/views/home.blade.php
+++ eLearning_app/resources/views/home.blade.php
@@ -2,19 +2,11 @@
 
 @section('content')
 <div class="container">
-<<<<<<< master
-<<<<<<< master
-=======
-<<<<<<< master
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-=======
->>>>>>> HEAD~3
-=======
 
->>>>>>> HEAD~0
     <div class="row">
         <!-- user profile -->
         <div class="col-sm-4">
@@ -26,14 +18,6 @@
                                  <img src="{{Auth::user()->avatar}}" style="width:100px;height:100px;">
                             </div>
                         </div>
-<<<<<<< master
-<<<<<<< master
-=======
->>>>>>> HEAD~2
-
->>>>>>> HEAD~3
-=======
->>>>>>> HEAD~0
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
